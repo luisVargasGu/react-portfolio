15c33c60f902ba7f05a6bedf1f96d6f3b9506d93
Luis Vargas <vargasluis012@gmail.com>
2023-12-14 08:20:21 -0500

    Side nav styling and layout complete.

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,8 +1,50 @@
 import './index.scss';
+import { Link, NavLink } from 'react-router-dom';
+import LogoS from '../../assets/images/logo-s.png';
+import LogoSubstitle from '../../assets/images/logo_sub.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 const Sidebar = () => {
     return (
-        <div>Hello from the Sidebar</div>
+        <div className='nav-bar'>
+            <Link className='logo' to="/">
+                <img src={LogoS} alt="logo"></img>
+                <img className="sub-logo" src={LogoSubstitle} alt="slobodan"></img>
+            </Link>
+            <nav>
+                <NavLink exact="true" activeclassname="active" to="/">
+                    <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+                </NavLink>
+                <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
+                    <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+                </NavLink>
+                <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
+                    <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+                </NavLink>
+            </nav>
+            <ul>
+                <li>
+                    <a
+                        target="_blank"
+                        rel="noreferrer"
+                        href="https://www.linkedin.com/in/luis-v-5122b8149/">
+                        <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e">
+                        </FontAwesomeIcon>
+                    </a>
+                </li>
+                <li>
+                    <a
+                        target="_blank"
+                        rel="noreferrer"
+                        href="https://github.com/luisVargasGu">
+                        <FontAwesomeIcon icon={faGithub} color="#4d4d4e">
+                        </FontAwesomeIcon>
+                    </a>
+                </li>
+            </ul>
+        </div>
     );
 }
 
