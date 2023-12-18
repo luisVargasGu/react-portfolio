cbe7555f8a1b5f849a878fa9e7f6ff60790920f7
Luis Vargas <vargasluis012@gmail.com>
2023-12-18 09:11:01 -0500

    Started tech stack cube

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,5 +1,7 @@
 import './index.scss';
 import { useEffect, useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCss3, faGitAlt, faGolang, faHtml5, faJsSquare, faReact } from '@fortawesome/free-brands-svg-icons';
 
 const About = () => {
     const carouselImages = [
@@ -32,11 +34,35 @@ const About = () => {
     return (
         <div className='about-container'>
             <div className='carousel-container'>
+                {/*TODO: Separate photos by size (width/height)*/}
                 <img src={carouselImages[currentImageIndex]}
                     alt={`Slide ${currentImageIndex + 1}`}
                     className={`carousel-image ${imageFadeClass}`} />
+                <div className='tech-stack-cube'>
+                    <div className='cubespinner'>
+                        <div className='face1'>
+                            <FontAwesomeIcon icon={faReact} color='#5ED4F4' />
+                        </div>
+                        <div className='face2'>
+                            <FontAwesomeIcon icon={faHtml5} color='#F06529' />
+                        </div>
+                        <div className='face3'>
+                            <FontAwesomeIcon icon={faCss3} color='#28A4D9' />
+                        </div>
+                        <div className='face4'>
+                            <FontAwesomeIcon icon={faJsSquare} color='#EFD81D' />
+                        </div>
+                        <div className='face5'>
+                            <FontAwesomeIcon icon={faGitAlt} color='#EC4D28' />
+                        </div>
+                        <div className='face6'>
+                            <FontAwesomeIcon icon={faGolang} color='#28A4D9' />
+                        </div>
+                    </div>
+                </div>
             </div>
             <div className="about-description">
+                <h3>About me</h3>
                 <p>
                     Welcome to my portfolio! I'm a seasoned Full Stack Developer with 5-7 years of experience, specializing in the front-end.
                     Proficient in Java, Python, and Go. (Currently learning O🐫)
