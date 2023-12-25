52c12398857fde0fc46293be91c6d2190aaee770
Luis Vargas <vargasluis012@gmail.com>
2023-12-24 20:42:46 -0500

    Finished UI for contact form UI.

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -3,7 +3,6 @@ import './index.scss';
 const Contact = () => {
     return (
         <div className='container contact-page'>
-            Test
             <div className='text-zone'>
                 <h1>Contact Me</h1>
                 <p>
@@ -12,6 +11,30 @@ const Contact = () => {
                     If you have any questions or would like to work together,
                     don't hesitate to reach out!
                 </p>
+                <div className='contact-form'>
+                    <form>
+                        <ul>
+                            <li className='half'>
+                                <input type='text' name='name' placeholder='Name' required>
+                                </input>
+                            </li>
+                            <li className='half'>
+                                <input type='email' name='email' placeholder='Email' required>
+                                </input>
+                            </li>
+                            <li>
+                                <input type='text' name='subject' placeholder='Subject' required>
+                                </input>
+                            </li>
+                            <li>
+                                <textarea name='message' placeholder='Message' required></textarea>
+                            </li>
+                            <li>
+                                <input type='submit' value='SEND' className='flat-button'></input>
+                            </li>
+                        </ul>
+                    </form>
+                </div>
             </div>
         </div>
     );
