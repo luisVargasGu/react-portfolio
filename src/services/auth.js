3fc2279016bd5516ab3e901728ccd45c9c792a21
Luis Vargas <vargasluis012@gmail.com>
2024-03-09 15:29:53 -0500

    Updated auth requets to handle JWT token in cookies.

--- src/services/auth.js
+++ src/services/auth.js
@@ -3,7 +3,9 @@ import { apiUrl } from "./environment";
 
 export const login = async (email, password) => {
     try {
-        const response = await axios.post(`${apiUrl}auth`, { username: email, password });
+        const response = await axios.post(`${apiUrl}auth`, { username: email, password }, {
+            withCredentials: true  // Add withCredentials flag
+        });
         return response.data;
     } catch (error) {
         console.error(error);
@@ -12,10 +14,11 @@ export const login = async (email, password) => {
 
 export const register = async (email, password) => {
     try {
-        const response = await axios.post(`${apiUrl}register`, { username: email, password });
+        const response = await axios.post(`${apiUrl}register`, { username: email, password }, {
+            withCredentials: true  // Add withCredentials flag
+        });
         return response.data;
     } catch (error) {
         console.error(error);
     }
 };
-
