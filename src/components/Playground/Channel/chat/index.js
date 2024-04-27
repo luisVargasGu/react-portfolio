8ef75267b8b9c5c48257d899a94bef551fcfaa21
Luis Vargas <vargasluis012@gmail.com>
2024-04-27 15:25:21 -0400

    Websocket connection in useEffect so it happens once and handles close.

--- src/components/Playground/Channel/chat/index.js
+++ src/components/Playground/Channel/chat/index.js
@@ -1,56 +1,63 @@
 import './index.scss';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import useAuthUser from 'react-auth-kit/hooks/useAuthUser';
-import { chat } from '../../../../services/chat';
+import { connectWebSocket, sendWebSocketMessage } from '../../../../services/chat';
 import Message from './message';
 
 
 const Chat = () => {
-    const [messages, setMessages] = useState([]);
-    const [input, setInput] = useState('');
-    const userData = useAuthUser();
+	const [messages, setMessages] = useState([]);
+	const [input, setInput] = useState('');
+	const userData = useAuthUser();
 
-    // Handle incoming messages from WebSocket
-    chat.onmessage = ({ data }) => {
-        const newMessage = JSON.parse(data);
-        setMessages([...messages, newMessage]);
-    }
+	useEffect(() => {
+		const chat = connectWebSocket('channels/1/room/1/messages');
 
-    const sendMessage = (e) => {
-        e.preventDefault();
-        if (input.trim() === '') return;
-        const newMessage = {
-            id: messages.length + 1,
-            text: input,
-            sender: userData.userID,
-            timestamp: new Date().toISOString()
-        }
+		chat.onmessage = ({ data }) => {
+			const newMessage = JSON.parse(data);
+			setMessages(prevMessages => [...prevMessages, newMessage]);
+		};
 
-        chat.send(JSON.stringify(newMessage));
-        setInput('');
-    }
+		return () => {
+			chat.close();
+		};
+	}, []);
 
-    return (
-        <div className='chat'>
-            <h1>Chat</h1>
-            <div className='chat-box'>
-                {messages.map((message) => (
-                    <Message message={message} key={message.id} />
-                ))}
-            </div>
-            <div className='chat-input'>
-                <form onSubmit={sendMessage}>
-                    <input
-                        type='text'
-                        value={input}
-                        onChange={(e) => setInput(e.target.value)}
-                        placeholder='Type your message...'
-                    />
-                </form>
-                <button onClick={sendMessage}>Send</button>
-            </div>
-        </div>
-    );
+	const sendMessage = (e) => {
+		e.preventDefault();
+		if (input.trim() === '') return;
+		const newMessage = {
+			id: messages.length + 1,
+			text: input,
+			sender: userData.userID,
+			timestamp: new Date().toISOString()
+		}
+
+		sendWebSocketMessage(newMessage);
+		setInput('');
+	}
+
+	return (
+		<div className='chat'>
+			<h1>Chat</h1>
+			<div className='chat-box'>
+				{messages.map((message) => (
+					<Message message={message} key={message.id} />
+				))}
+			</div>
+			<div className='chat-input'>
+				<form onSubmit={sendMessage}>
+					<input
+						type='text'
+						value={input}
+						onChange={(e) => setInput(e.target.value)}
+						placeholder='Type your message...'
+					/>
+				</form>
+				<button onClick={sendMessage}>Send</button>
+			</div>
+		</div>
+	);
 }
 
 export default Chat;
