b86fbd95de979bb6c8e5115d37b946c1aba4213f
Luis Vargas <vargasluis012@gmail.com>
2024-06-02 20:31:44 -0400

    move timedifference function to a service file.

--- src/components/Playground/Channel/chat/message/index.js
+++ src/components/Playground/Channel/chat/message/index.js
@@ -1,58 +1,29 @@
-import './index.scss';
-import React, { useEffect, useState } from 'react';
+import './index.scss'
+import React, { useEffect, useState } from 'react'
+import { getTimeDifference } from '../../../../../services/dates'
 
 const Message = ({ message }) => {
-	const [timeDifference, setTimeDifference] = useState(getTimeDifference());
+    const [timeDifference, setTimeDifference] = useState(
+        getTimeDifference(message)
+    )
 
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setTimeDifference(getTimeDifference(message))
+        }, 60000)
 
-	function getTimeDifference() {
-		const currentTime = new Date();
-		const messageTime = new Date(message.timestamp);
-		const difference = currentTime - messageTime;
-		const intervals = [
-			{ label: 'minute', duration: 60 * 1000 },
-			{ label: 'hour', duration: 60 * 60 * 1000 },
-			{ label: 'day', duration: 24 * 60 * 60 * 1000 },
-			{ label: 'week', duration: 7 * 24 * 60 * 60 * 1000 },
-			{ label: 'month', duration: 30 * 24 * 60 * 60 * 1000 },
-			{ label: 'year', duration: 365 * 24 * 60 * 60 * 1000 }
-		];
+        return () => clearInterval(interval)
+    })
 
-		for (let i = 0; i < intervals.length; i++) {
-			const interval = intervals[i];
-			const numIntervals = Math.floor(difference / interval.duration);
+    return (
+        <div className="message-box" key={message.id}>
+            <div className="sender-circle">{message.sender}</div>
+            <div className="message-content">
+                <p>{message.content}</p>
+                <span>{timeDifference}</span>
+            </div>
+        </div>
+    )
+}
 
-			if (numIntervals === 0) {
-				return 'Just now';
-			} else if (numIntervals === 1) {
-				return `1 ${interval.label} ago`;
-			} else if (numIntervals === 2) {
-				return `2 ${interval.label}s ago`;
-			} else if (numIntervals < 24) {
-				return `${numIntervals} ${interval.label}s ago`;
-			}
-		}
-
-		return 'Long time ago';
-	};
-
-	useEffect(() => {
-		const interval = setInterval(() => {
-			setTimeDifference(getTimeDifference());
-		}, 60000);
-
-		return () => clearInterval(interval);
-	}, []);
-
-	return (
-		<div className='message-box' key={message.id}>
-			<div className="sender-circle">{message.sender}</div>
-			<div className='message-content'>
-				<p>{message.text}</p>
-				<span>{timeDifference}</span>
-			</div>
-		</div>
-	)
-};
-
-export default Message;
+export default Message
