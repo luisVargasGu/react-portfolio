a833e6a20e40f80e17bdce76ff069c5db165e5d8
Luis Vargas <vargasluis012@gmail.com>
2024-06-05 07:33:25 -0400

    Making ChannelList component styles match rest of app.

--- src/components/Sidebar/ChannelList/index.js
+++ src/components/Sidebar/ChannelList/index.js
@@ -1,42 +1,54 @@
-import React, { useEffect } from 'react';
-import { connect, useDispatch } from 'react-redux';
-import { NavLink } from 'react-router-dom';
-import { updateSelectedChannel, fetchChannels } from '../../../store/channels/channels.actions';
+import { faPlus } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import React, { useEffect } from 'react'
+import { connect, useDispatch } from 'react-redux'
+import { NavLink } from 'react-router-dom'
+import {
+    updateSelectedChannel,
+    fetchChannels,
+} from '../../../store/channels/channels.actions'
 
 const ChannelList = ({ channels, fetchChannels }) => {
-    const dispatch = useDispatch();
+    const dispatch = useDispatch()
 
     useEffect(() => {
-        fetchChannels();
-    }, [fetchChannels]);
+        fetchChannels()
+    }, [fetchChannels])
 
     const handleChannelClick = (channelId) => {
-        dispatch(updateSelectedChannel(channelId));
+        dispatch(updateSelectedChannel(channelId))
+    }
+
+    const addChannel = () => {
+        console.log('add channel')
     }
 
     return (
         <nav>
-            {channels?.channels?.map((channel) => (
-                <NavLink
+            <button className="channel-link" onClick={() => addChannel()}>
+                <h3>
+                    <FontAwesomeIcon icon={faPlus} color="#fff" />
+                </h3>
+            </button>
+            {channels?.channels.map((channel) => (
+                <button
                     key={channel.ID}
-                    exact="true"
-                    activeclassname="active"
-                    className="channel-link"
-                    onClick={() => handleChannelClick(channel.ID)}>
+                    className={`channel-link ${channels.selectedChannelId === channel.ID ? 'active' : ''}`}
+                    onClick={() => handleChannelClick(channel.ID)}
+                >
                     <h3>{channel.ID}</h3>
-                </NavLink>
+                </button>
             ))}
         </nav>
-
-    );
+    )
 }
 
 const mapStateToProps = (state) => ({
     channels: state.channels,
-});
+})
 
 const mapDispatchToProps = {
     fetchChannels,
-};
+}
 
-export default connect(mapStateToProps, mapDispatchToProps)(ChannelList);
+export default connect(mapStateToProps, mapDispatchToProps)(ChannelList)
