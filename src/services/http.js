0be320d82ca46c728b717151b4626c2f6db1d41f
Luis Vargas <vargasluis012@gmail.com>
2024-06-09 23:23:16 -0400

    Deleted un-used code

--- src/services/http.js
+++ src/services/http.js
@@ -1,13 +1,3 @@
-import axios from "axios";
-
-function attachAuthorizationHeader(request) {
-    const token = getCookie('jwt_token');
-    if (token) {
-        request.headers['Authorization'] = `Bearer ${token}`;
-    }
-    return request;
-}
-
 export const getCookie = (name) => {
     const cookies = Object.fromEntries(
         document.cookie.split("; ").map((c) => c.split("="))
@@ -15,38 +5,3 @@ export const getCookie = (name) => {
     return cookies[name];
 }
 
-export const getWithAuth = async (url, config = {}) => {
-    try {
-        const response = await axios.get(url, attachAuthorizationHeader(config));
-        return response.data;
-    } catch (error) {
-        console.error(error);
-    }
-};
-
-export const postWithAuth = async (url, data, config = {}) => {
-    try {
-        const response = await axios.post(url, data, attachAuthorizationHeader(config));
-        return response.data;
-    } catch (error) {
-        console.error(error);
-    }
-};
-
-export const putWithAuth = async (url, data, config = {}) => {
-    try {
-        const response = await axios.put(url, data, attachAuthorizationHeader(config));
-        return response.data;
-    } catch (error) {
-        console.error(error);
-    }
-};
-
-export const deleteWithAuth = async (url, config = {}) => {
-    try {
-        const response = await axios.delete(url, attachAuthorizationHeader(config));
-        return response.data;
-    } catch (error) {
-        console.error(error);
-    }
-};
