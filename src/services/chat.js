106498e50d00c43347d275787bc7f4a914a498ea
Luis Vargas <vargasluis012@gmail.com>
2024-06-09 23:23:47 -0400

    Style changes.

--- src/services/chat.js
+++ src/services/chat.js
@@ -1,24 +1,23 @@
-import { webSocketURL } from "./environment";
+import { webSocketURL } from './environment'
 
-let socket;
+let socket
 
 export const connectWebSocket = (target) => {
-    socket = new WebSocket(webSocketURL + target);
-    return socket;
-};
+    socket = new WebSocket(webSocketURL + target)
+    return socket
+}
 
 export const disconnectWebSocket = () => {
     if (socket) {
-        socket.close(1000, 'User disconnected');
+        socket.close(1000, 'User disconnected')
     }
-    socket = null;
-};
+    socket = null
+}
 
 export const sendWebSocketMessage = (message) => {
     if (socket && socket.readyState === WebSocket.OPEN) {
-        socket.send(JSON.stringify(message));
+        socket.send(JSON.stringify(message))
     } else {
-        console.error('WebSocket is not connected');
+        console.error('WebSocket is not connected')
     }
-};
-
+}
