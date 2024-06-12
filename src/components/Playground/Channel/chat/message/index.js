20466c5241dcb78c502233e4c1ce2e387bb601e6
Luis Vargas <vargasluis012@gmail.com>
2024-06-12 00:16:03 -0400

    Added seen by to messages and associated styling.

--- src/components/Playground/Channel/chat/message/index.js
+++ src/components/Playground/Channel/chat/message/index.js
@@ -1,11 +1,15 @@
 import './index.scss'
 import React, { useEffect, useState } from 'react'
 import { getTimeDifference } from '../../../../../services/dates'
+import useMessageObserver from '../../../../../services/screen'
 
-const Message = ({ message }) => {
+// TODO: don't mark as seen if already marked
+// TODO: don't mark as seen if you are the sender
+const Message = ({ root, message }) => {
     const [timeDifference, setTimeDifference] = useState(
         getTimeDifference(message)
     )
+    const messageRef = useMessageObserver(message.id, root, 1)
 
     useEffect(() => {
         const interval = setInterval(() => {
@@ -16,7 +20,7 @@ const Message = ({ message }) => {
     })
 
     return (
-        <div className="message-box" key={message.id}>
+        <div className="message-box" ref={messageRef} key={message.id}>
             <div className="sender-circle">
                 {!message.sender_avatar ?
                     (
@@ -30,7 +34,21 @@ const Message = ({ message }) => {
             <div className="message-content">
                 <h4>{message.sender_name}</h4>
                 <p>{message.content}</p>
-                <span>{timeDifference}</span>
+                <div class="seen_by">
+                    {message?.seen_by?.map(({ avatar, username }) => (
+                        <div className="sender-circle">
+                            {!avatar ?
+                                (
+                                    <div className="default-avatar">
+                                        <span>{username}</span>
+                                    </div>
+                                ) :
+                                (<img src={'data:image/png;base64,' + avatar} alt="User-Avatar" />)
+                            }
+                        </div>
+                    ))}
+                </div>
+                <span className="time-stamp">{timeDifference}</span>
             </div>
         </div>
     )
