1e622be132e522c65a8674040e44827236aeeb1c
Luis Vargas <vargasluis012@gmail.com>
2024-06-15 13:34:20 -0400

    Update jest config to handle images with stub npm package.

--- jest.config.js
+++ jest.config.js
@@ -9,9 +9,10 @@ module.exports = {
 		'^.+\\.(ts|tsx)?$': 'ts-jest',
 		'^.+\\.(js|jsx)$': 'babel-jest',
 		'^.+\\.mjs$': 'babel-jest',
-		// '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/mocks/fileMock.js'
 	},
-	transformIgnorePatterns: ['<rootDir>/node_modules/'],
+	transformIgnorePatterns: [
+		'<rootDir>/node_modules/',
+	],
 	moduleNameMapper: {
 		'^@/(.*)$': '<rootDir>/src/$1',
 		'^@components/(.*)$': '<rootDir>/src/components/$1',
@@ -19,7 +20,7 @@ module.exports = {
 		'^@services/(.*)$': '<rootDir>/src/services/$1',
 		'^@store/(.*)$': '<rootDir>/src/store/$1',
 		'^@assets/(.*)$': '<rootDir>/src/assets/$1',
-		'\\.(jpg|jpeg|png|gif|svg)$': '<rootDir>/mocks/fileMock.js',
+		'\\.(jpg|jpeg|png|gif|webp|svg)$': 'jest-transform-stub',
 		'\\.(css|less|scss|sass)$': 'identity-obj-proxy',
 	},
 }
